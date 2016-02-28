c2fce37f97920ac8c741db3f858a99e2ece7415b
Arun Allamsetty <arun@instructure.com>
2016-02-27 17:54:41 -0700

    Hiding helper methods using block scope.

--- src/main/scala/org/coursera/progfun/week1/SquareRoot.scala
+++ src/main/scala/org/coursera/progfun/week1/SquareRoot.scala
@@ -7,19 +7,21 @@ import scala.annotation.tailrec
 object Math {
   def abs(n: Double) = if (n < 0) -n else n
 
-  @tailrec
-  def sqrtIter(guess: Double, n: Double): Double = {
-    if (isGoodEnough(guess, n)) guess
-    else sqrtIter(improve(guess, n), n)
-  }
+  def sqrt(n: Double): Double = {
+    @tailrec
+    def sqrtIter(guess: Double): Double = {
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
+    }
 
-  def isGoodEnough(guess: Double, n: Double): Boolean = {
-    abs(guess * guess - n) / n < 0.001
-  }
+    def isGoodEnough(guess: Double): Boolean = {
+      abs(guess * guess - n) / n < 0.001
+    }
 
-  def improve(guess: Double, n: Double): Double = {
-    (guess + n / guess) / 2
-  }
+    def improve(guess: Double): Double = {
+      (guess + n / guess) / 2
+    }
 
-  def sqrt(n: Double): Double = sqrtIter(1.0, n)
+    sqrtIter(1.0)
+  }
 }
