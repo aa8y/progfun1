7b1e11b4f3b5dd7f5b1b5441a517ef6a098b4829
Arun Allamsetty <arun@instructure.com>
2016-02-27 18:42:23 -0700

    Added factorial and gcd functions.

--- src/main/scala/org/coursera/progfun/week1/Run.scala
+++ src/main/scala/org/coursera/progfun/week1/Run.scala
@@ -5,5 +5,21 @@ import org.coursera.progfun.week1.Math._
 
 
 object Week1Run extends App {
-  Seq(2, 1e-6, 1e60).map(sqrt).foreach(println)
+  println("Factorial of:")
+  Seq(0, 5).foreach { n =>
+    println(s"$n = ${factorial(n)}")
+  }
+  println()
+
+  println("GCD of:")
+  Seq((0, 14), (21, 14), (14, 21)).foreach {
+    case (x: Int, y: Int) => println(s"($x, $y) = ${gcd(x, y)}")
+  }
+  println()
+
+  println("Square root of:")
+  Seq(2, 1e-6, 1e60).foreach { n =>
+    println(s"$n = ${sqrt(n)}")
+  }
+  println()
 }
--- src/main/scala/org/coursera/progfun/week1/SquareRoot.scala
+++ src/main/scala/org/coursera/progfun/week1/SquareRoot.scala
@@ -7,6 +7,17 @@ import scala.annotation.tailrec
 object Math {
   def abs(n: Double) = if (n < 0) -n else n
 
+  def factorial(n: Int): Long = {
+    if (n == 0) 1
+    else n * factorial(n - 1)
+  }
+
+  @tailrec
+  def gcd(x: Int, y: Int): Int = {
+    if (y == 0) x
+    else gcd(y, x % y)
+  }
+
   def sqrt(n: Double): Double = {
     @tailrec
     def sqrtIter(guess: Double): Double = {
