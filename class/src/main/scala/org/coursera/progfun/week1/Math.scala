5c19a6bab7cb48b59f94ed959f2f654035629e35
Arun Allamsetty <arun@instructure.com>
2016-06-20 11:48:33 -0600

    Fix existing class code.

--- class/src/main/scala/org/coursera/progfun/week1/Math.scala
+++ class/src/main/scala/org/coursera/progfun/week1/Math.scala
@@ -1,8 +1,28 @@
 package org.coursera.progfun.week1
 
-
 import scala.annotation.tailrec
 
+object Run extends App {
+  import Math._
+
+  println("Factorial of:")
+  Seq(0, 5).foreach { n =>
+    println(s"$n = ${factorial(n)}")
+  }
+  println()
+
+  println("GCD of:")
+  Seq((0, 14), (21, 14), (14, 21)).foreach {
+    case (x: Int, y: Int) => println(s"($x, $y) = ${gcd(x, y)}")
+  }
+  println()
+
+  println("Square root of:")
+  Seq(2, 1e-6, 1e60).foreach { n =>
+    println(s"$n = ${sqrt(n)}")
+  }
+  println()
+}
 
 object Math {
   def abs(n: Double) = if (n < 0) -n else n
