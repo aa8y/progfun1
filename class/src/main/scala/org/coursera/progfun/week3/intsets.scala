1b225d0f59dab6d294e1b9ae99034c0bb603564c
Arun Allamsetty <arun@instructure.com>
2016-06-23 18:45:44 -0600

    A non-complete solution to the equals problem.

--- class/src/main/scala/org/coursera/progfun/week3/intsets.scala
+++ class/src/main/scala/org/coursera/progfun/week3/intsets.scala
@@ -10,6 +10,8 @@ abstract class IntSet {
 
   def size: Int
 
+  def intersect(other: IntSet): IntSet
+
   def union(other: IntSet): IntSet
 }
 
@@ -21,9 +23,16 @@ class NonEmpty(val n: Int, val left: IntSet = Empty, val right: IntSet = Empty) 
   }
 
   def incl(x: Int): IntSet = {
-    if (x < n) left incl x
-    else if (x > n) right incl x
+    if (x < n) new NonEmpty(n, left incl x, right)
+    else if (x > n) new NonEmpty(n, left, right incl x)
     else this
+  }
+
+  def intersect(other: IntSet): IntSet = other match {
+    case that: NonEmpty => 
+      val inter = (left intersect right) intersect that
+      if (that.contains(n)) inter incl n else inter
+    case _ => Empty
   }
 
   def size: Int = left.size + 1 + right.size
@@ -31,7 +40,14 @@ class NonEmpty(val n: Int, val left: IntSet = Empty, val right: IntSet = Empty) 
   def union(other: IntSet): IntSet = ((left union right) union other) incl n
 
   override def equals(other: Any): Boolean = other match {
-    case o: NonEmpty => o.n == this.n && o.left == this.left && o.right == this.right
+    case that: NonEmpty =>
+      if (this.n > that.n) {
+        this.left == new NonEmpty(that.n, that.left, Empty) && 
+        new NonEmpty(this.n, Empty, this.right) == that.right
+      } else if (this.n < that.n) {
+        new NonEmpty(this.n, this.left, Empty) == that.left && 
+        this.right == new NonEmpty(that.n, Empty, that.right)
+      } else that.left == this.left && that.right == this.right
     case _ => false
   }
 
@@ -42,6 +58,8 @@ class Empty extends IntSet {
   def contains(x: Int): Boolean = false
 
   def incl(x: Int): IntSet = new NonEmpty(x)
+
+  def intersect(other: IntSet): IntSet = this
 
   def size: Int = 0
 
--- class/src/test/scala/org/coursera/progfun/week3/IntSetTest.scala
+++ class/src/test/scala/org/coursera/progfun/week3/IntSetTest.scala
@@ -22,7 +22,30 @@ class IntSetTest extends FlatSpec with Matchers {
     (Empty union s2) should be (s2)
   }
 
+  it should "return itself when intersected with any other set." in {
+    (Empty intersect Empty) should be (Empty)
+    (Empty intersect s1) should be (Empty)
+  }
+
   "A non-empty set" should "return the same non-empty set when unioned with an empty set." in {
     (s1 union Empty) should be (s1)
+  }
+
+  it should "return an empty set when intersected with an empty set." in {
+    (s1 intersect Empty) should be (Empty)
+  }
+
+  it should "return an empty set when intersected with a non-empty set with no common elements." in {
+    (s1 intersect s2) should be (Empty)
+  }
+
+  it should "be equal to another non-empty set even when the insertion order is different." in {
+    val s1 = ((new NonEmpty(1) incl 2) incl 3)
+    val s2 = ((new NonEmpty(2) incl 1) incl 3)
+    val s3 = ((new NonEmpty(3) incl 2) incl 1)
+
+    (s1) should be (s2)
+    (s2) should be (s3)
+    (s3) should be (s1)
   }
 }
